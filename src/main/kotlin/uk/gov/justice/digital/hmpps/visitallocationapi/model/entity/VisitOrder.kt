ebb480a660f0e7de98a1d5cd71d8f941560ceaa6
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-01-20 14:11:28 +0000

    VB-5074 - Create visit order table, flyway script and entity

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/model/entity/VisitOrder.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.visitallocationapi.model.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.CreatedType
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.VisitOrderStatus
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.VisitOrderType
+import java.time.LocalDate
+
+@Entity
+@Table(name = "VISIT_ORDER")
+data class VisitOrder(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Long = 0L,
+
+  @Column(nullable = false)
+  val prisonerId: String,
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  val createdType: CreatedType,
+
+  @Column(nullable = false)
+  val createdBy: String,
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  val type: VisitOrderType,
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
+  val status: VisitOrderStatus,
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  val staffComment: String,
+
+  @Column(nullable = false)
+  val createdDate: LocalDate = LocalDate.now(),
+
+  @Column(nullable = false)
+  val expiryDate: LocalDate? = null,
+
+  @Column(nullable = false)
+  val outcomeReasonCode: String? = null,
+
+  @Column(nullable = false)
+  val mailedDate: LocalDate? = null,
+)
---
+++ src/main/resources/db/migration/V1_0__create_visit_order_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE visit_order
+(
+    id                      serial          NOT NULL PRIMARY KEY,
+    prisoner_id             VARCHAR(80)     NOT NULL,
+    created_type            VARCHAR(20)     NOT NULL,
+    created_by              VARCHAR(80)     NOT NULL,
+    type                    VARCHAR(20)     NOT NULL,
+    status                  VARCHAR(20)     NOT NULL,
+    staff_comment           TEXT            NOT NULL,
+    created_date            DATE            default current_date,
+    expiry_date             DATE,
+    outcome_reason_code     VARCHAR(20),
+    mailed_date             DATE
+);
