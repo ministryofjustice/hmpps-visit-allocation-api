3eaa89c81f1d53d99a421b3043d4448f569e9a02
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-01-23 13:27:44 +0000

    Make integration test more specific

--- src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/domainevents/EventsSqsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/domainevents/EventsSqsTest.kt
@@ -9,6 +9,7 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import uk.gov.justice.digital.hmpps.visitallocationapi.service.DomainEventListenerService.Companion.CONVICTION_STATUS_UPDATED_EVENT_TYPE
+import uk.gov.justice.digital.hmpps.visitallocationapi.service.listener.events.DomainEvent
 import uk.gov.justice.hmpps.sqs.countMessagesOnQueue
 
 class EventsSqsTest : EventsIntegrationTestBase() {
@@ -28,6 +29,6 @@ class EventsSqsTest : EventsIntegrationTestBase() {
     // Then
     await untilCallTo { sqsClient.countMessagesOnQueue(queueUrl).get() } matches { it == 0 }
     await untilAsserted { verify(domainEventListenerSpy, times(1)).processMessage(any()) }
-    await untilAsserted { verify(prisonerConvictionStatusUpdatedProcessorSpy, times(1)).processEvent(any()) }
+    await untilAsserted { verify(prisonerConvictionStatusUpdatedProcessorSpy, times(1)).processEvent(objectMapper.readValue(domainEvent, DomainEvent::class.java)) }
   }
 }
