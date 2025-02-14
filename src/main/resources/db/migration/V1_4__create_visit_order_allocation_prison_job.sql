c745ff57e03179d7c0e17dbe539f7512a94b81b3
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-02-14 12:43:28 +0000

    Updates for review comments.

--- src/main/resources/db/migration/V1_4__create_visit_order_allocation_prison_job.sql
+++ src/main/resources/db/migration/V1_4__create_visit_order_allocation_prison_job.sql
@@ -5,5 +5,11 @@ CREATE TABLE visit_order_allocation_prison_job
     prison_code                     varchar(3) NOT NULL ,
     create_timestamp                timestamp       NOT NULL,
     start_timestamp                 timestamp,
-    end_timestamp                   timestamp
+    end_timestamp                   timestamp,
+    failure_message                 VARCHAR(100),
+    convicted_prisoners             int,
+    processed_prisoners             int,
+    failed_prisoners                int
 );
+
+CREATE INDEX visit_order_allocation_prison_job_ref_prison_code ON visit_order_allocation_prison_job(allocation_job_reference, prison_code);
