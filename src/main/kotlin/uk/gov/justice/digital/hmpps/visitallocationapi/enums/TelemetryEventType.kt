ccfa800db610b5f4135ddff8c57dbf4fa4e04920
dhirajshettymoj <dhiraj.shetty@digital.justice.gov.uk>
2025-03-13 16:06:20 +0000

    Add support for appinsights

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/enums/TelemetryEventType.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.visitallocationapi.enums
+
+enum class TelemetryEventType(val telemetryEventName: String) {
+  // TODO - sample value - might need updating when implemented
+  BALANCES_OUT_OF_SYNC("balances_out_of_sync"),
+}
