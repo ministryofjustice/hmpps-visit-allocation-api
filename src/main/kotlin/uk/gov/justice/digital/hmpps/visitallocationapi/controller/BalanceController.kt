7814dc02c18f9cc04c9b177ad865c87581443396
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-03-20 10:04:24 +0000

    Add a balance endpoint to visit-allocation-api
    Allow for prisoner balance to be retrieved.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/controller/BalanceController.kt
@@ -0,0 +1,46 @@
+package uk.gov.justice.digital.hmpps.visitallocationapi.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.visitallocationapi.dto.PrisonerBalanceDto
+import uk.gov.justice.digital.hmpps.visitallocationapi.service.BalanceService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+const val VO_BALANCE = "/visits/allocation/prisoner/{prisonerId}/balance"
+
+@RestController
+class BalanceController(val balanceService: BalanceService) {
+  @PreAuthorize("hasRole('ROLE_VISIT_ALLOCATION_API__PRISONER_BALANCE')")
+  @GetMapping(VO_BALANCE)
+  @Operation(
+    summary = "Endpoint to get a prisoners current balance.",
+    description = "Takes a prisoner id and return their current visit order balance.",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Prisoner balance returned.",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Incorrect permissions to get prisoner balance.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  fun getPrisonerBalance(
+    @Schema(description = "prisonerId", example = "AA123456", required = true)
+    @PathVariable
+    prisonerId: String,
+  ): PrisonerBalanceDto =return balanceService.getPrisonerBalance(prisonerId)
+}
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/BalanceControllerTest.kt
@@ -0,0 +1,115 @@
+package uk.gov.justice.digital.hmpps.visitallocationapi.integration
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec
+import uk.gov.justice.digital.hmpps.visitallocationapi.controller.VO_BALANCE
+import uk.gov.justice.digital.hmpps.visitallocationapi.dto.PrisonerBalanceDto
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.NegativeVisitOrderStatus
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.NegativeVisitOrderType
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.VisitOrderStatus
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.VisitOrderType
+import uk.gov.justice.digital.hmpps.visitallocationapi.integration.helper.callGet
+import uk.gov.justice.digital.hmpps.visitallocationapi.model.entity.NegativeVisitOrder
+import uk.gov.justice.digital.hmpps.visitallocationapi.model.entity.VisitOrder
+
+@DisplayName("Balance Controller tests - $VO_BALANCE")
+class BalanceControllerTest : IntegrationTestBase() {
+
+  companion object {
+    const val PRISONER_ID = "AA123456"
+    const val AUTH_ROLES = "ROLE_VISIT_ALLOCATION_API__PRISONER_BALANCE"
+  }
+
+  @Test
+  fun `when request to get existing prisoner with positive balance, then return prisoner balance`() {
+    // Given
+    visitOrderRepository.save(VisitOrder(prisonerId = PRISONER_ID, type = VisitOrderType.VO, status = VisitOrderStatus.AVAILABLE))
+    visitOrderRepository.save(VisitOrder(prisonerId = PRISONER_ID, type = VisitOrderType.VO, status = VisitOrderStatus.EXPIRED))
+    visitOrderRepository.save(VisitOrder(prisonerId = PRISONER_ID, type = VisitOrderType.PVO, status = VisitOrderStatus.AVAILABLE))
+
+    // When
+    val responseSpec = callVisitAllocationPrisonerBalanceEndpoint(PRISONER_ID, webTestClient, setAuthorisation(roles = listOf(AUTH_ROLES)))
+
+    // Then
+    responseSpec.expectStatus().isOk
+
+    val prisonerBalance = getVoBalanceResponse(responseSpec)
+
+    assertThat(prisonerBalance.voBalance).isEqualTo(1)
+    assertThat(prisonerBalance.pvoBalance).isEqualTo(1)
+  }
+
+  @Test
+  fun `when request to get existing prisoner with negative balance, then return prisoner balance`() {
+    // Given
+    negativeVisitOrderRepository.save(NegativeVisitOrder(prisonerId = PRISONER_ID, type = NegativeVisitOrderType.NEGATIVE_VO, status = NegativeVisitOrderStatus.USED))
+    negativeVisitOrderRepository.save(NegativeVisitOrder(prisonerId = PRISONER_ID, type = NegativeVisitOrderType.NEGATIVE_PVO, status = NegativeVisitOrderStatus.USED))
+
+    // When
+    val responseSpec = callVisitAllocationPrisonerBalanceEndpoint(PRISONER_ID, webTestClient, setAuthorisation(roles = listOf(AUTH_ROLES)))
+
+    // Then
+    responseSpec.expectStatus().isOk
+
+    val prisonerBalance = getVoBalanceResponse(responseSpec)
+
+    assertThat(prisonerBalance.voBalance).isEqualTo(-1)
+    assertThat(prisonerBalance.pvoBalance).isEqualTo(-1)
+  }
+
+  @Test
+  fun `when request to get an unknown prisoner, then return empty (zero) balance`() {
+    // Given
+    // When
+    val responseSpec = callVisitAllocationPrisonerBalanceEndpoint(PRISONER_ID, webTestClient, setAuthorisation(roles = listOf(AUTH_ROLES)))
+
+    // Then
+    responseSpec.expectStatus().isOk
+
+    val prisonerBalance = getVoBalanceResponse(responseSpec)
+
+    assertThat(prisonerBalance.voBalance).isEqualTo(0)
+    assertThat(prisonerBalance.pvoBalance).isEqualTo(0)
+  }
+
+  @Test
+  fun `access forbidden when no role`() {
+    // Given
+    val incorrectAuthHeaders = setAuthorisation(roles = listOf())
+
+    // When
+    val responseSpec = callVisitAllocationPrisonerBalanceEndpoint(PRISONER_ID, webTestClient, incorrectAuthHeaders)
+
+    // Then
+    responseSpec.expectStatus().isForbidden
+  }
+
+  @Test
+  fun `unauthorised when no token`() {
+    // Given no auth token
+
+    // When
+    val responseSpec = webTestClient.get().uri(getPrisonerBalanceUrl(PRISONER_ID)).exchange()
+
+    // Then
+    responseSpec.expectStatus().isUnauthorized
+  }
+
+  private fun callVisitAllocationPrisonerBalanceEndpoint(
+    prisonerId: String,
+    webTestClient: WebTestClient,
+    authHttpHeaders: (HttpHeaders) -> Unit,
+  ): ResponseSpec = callGet(
+    webTestClient,
+    getPrisonerBalanceUrl(prisonerId),
+    authHttpHeaders,
+  )
+
+  private fun getPrisonerBalanceUrl(prisonerId: String): String = VO_BALANCE.replace("{prisonerId}", prisonerId)
+
+  private fun getVoBalanceResponse(responseSpec: ResponseSpec): PrisonerBalanceDto = objectMapper.readValue(responseSpec.expectBody().returnResult().responseBody, PrisonerBalanceDto::class.java)
+}
