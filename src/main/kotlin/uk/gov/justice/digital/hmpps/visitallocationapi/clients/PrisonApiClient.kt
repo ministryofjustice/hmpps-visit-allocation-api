acc8da37366352da80c9fdb13c11fc9ec5979792
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-05-14 11:40:40 +0100

    Fix incorrect import
    The import was wrong, causing the when() check to fail and not match a
    NO_CONTENT response, which caused a 204 + exception

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/clients/PrisonApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/clients/PrisonApiClient.kt
@@ -1,10 +1,10 @@
 package uk.gov.justice.digital.hmpps.visitallocationapi.clients
 
-import io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
