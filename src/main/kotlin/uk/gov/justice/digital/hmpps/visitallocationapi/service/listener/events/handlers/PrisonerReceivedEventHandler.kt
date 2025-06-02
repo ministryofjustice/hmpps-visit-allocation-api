6d9f627345d612c7d25d89bc97aef87f1936357f
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-06-02 10:24:17 +0100

    VB-5126 - Wipe prisoner balance for certain PrisonerReceived event reasons
    When a prisoner received event is processed, if the reason is within a list of "Should wipe" reasons,
    we reset the prisoners balance to zero for a DPS prison (even if it's negative).

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/service/listener/events/handlers/PrisonerReceivedEventHandler.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/service/listener/events/handlers/PrisonerReceivedEventHandler.kt
@@ -1,10 +1,18 @@
 package uk.gov.justice.digital.hmpps.visitallocationapi.service.listener.events.handlers
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.visitallocationapi.enums.DomainEventType
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.nomis.PrisonerReceivedReasonType
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.nomis.PrisonerReceivedReasonType.NEW_ADMISSION
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.nomis.PrisonerReceivedReasonType.POST_MERGE_ADMISSION
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.nomis.PrisonerReceivedReasonType.READMISSION
+import uk.gov.justice.digital.hmpps.visitallocationapi.enums.nomis.PrisonerReceivedReasonType.READMISSION_SWITCH_BOOKING
 import uk.gov.justice.digital.hmpps.visitallocationapi.service.NomisSyncService
 import uk.gov.justice.digital.hmpps.visitallocationapi.service.PrisonService
+import uk.gov.justice.digital.hmpps.visitallocationapi.service.ProcessPrisonerService
+import uk.gov.justice.digital.hmpps.visitallocationapi.service.SnsService
 import uk.gov.justice.digital.hmpps.visitallocationapi.service.listener.events.DomainEvent
 import uk.gov.justice.digital.hmpps.visitallocationapi.service.listener.events.additionalinfo.PrisonerReceivedInfo
 
@@ -13,7 +21,13 @@ class PrisonerReceivedEventHandler(
   objectMapper: ObjectMapper,
   private val prisonService: PrisonService,
   private val nomisSyncService: NomisSyncService,
+  private val processPrisonerService: ProcessPrisonerService,
+  private val snsService: SnsService,
 ) : DomainEventHandler {
+
+  companion object {
+    private val LOG = LoggerFactory.getLogger(this::class.java)
+  }
 
   private val processor = StandardDomainEventHandler(
     objectMapper = objectMapper,
@@ -30,13 +44,31 @@ class PrisonerReceivedEventHandler(
 
   private fun shouldProcess(info: PrisonerReceivedInfo): Boolean = true
 
-  private fun isDpsPrison(info: PrisonerReceivedInfo): Boolean = prisonService.getPrisonEnabledForDpsByCode(info.prisonCode)
+  private fun isDpsPrison(info: PrisonerReceivedInfo): Boolean {
+    if (prisonService.getPrisonEnabledForDpsByCode(info.prisonCode)) {
+      LOG.info("Prison ${info.prisonCode} is enabled for DPS, processing event")
+      return true
+    } else {
+      LOG.info("Prison ${info.prisonCode} is disabled for DPS, skipping event")
+      return false
+    }
+  }
 
   private fun processDps(info: PrisonerReceivedInfo) {
-    // TODO Implement.
+    if (shouldWipePrisonerBalance(info.reason)) {
+      LOG.info("Prisoner ${info.prisonerId} received for reason ${info.reason}, wiping balance")
+      val changeLog = processPrisonerService.processPrisonerReceivedResetBalance(info.prisonerId, info.reason)
+      if (changeLog != null) {
+        snsService.sendPrisonAllocationAdjustmentCreatedEvent(changeLog)
+      }
+    } else {
+      LOG.info("Prisoner ${info.prisonerId} received for reason ${info.reason}, not wiping balance")
+    }
   }
 
   private fun processNomis(info: PrisonerReceivedInfo) {
     nomisSyncService.syncPrisonerBalanceFromEventChange(info.prisonerId, DomainEventType.PRISONER_RECEIVED_EVENT_TYPE)
   }
+
+  private fun shouldWipePrisonerBalance(reason: PrisonerReceivedReasonType): Boolean = reason in listOf(POST_MERGE_ADMISSION, NEW_ADMISSION, READMISSION_SWITCH_BOOKING, READMISSION)
 }
