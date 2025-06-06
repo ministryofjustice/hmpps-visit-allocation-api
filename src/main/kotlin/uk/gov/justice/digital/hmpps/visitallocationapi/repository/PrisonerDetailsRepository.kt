6d27c71ca4c2a728fd9a2c8cb39b1cf34b4c9519
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-06-06 11:47:51 +0100

    add 5second timeout to lock wait

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/repository/PrisonerDetailsRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/repository/PrisonerDetailsRepository.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.visitallocationapi.repository
 
 import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.visitallocationapi.model.entity.PrisonerDetails
 import java.util.*
@@ -12,6 +14,7 @@ import java.util.*
 interface PrisonerDetailsRepository : JpaRepository<PrisonerDetails, String> {
 
   @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @QueryHints(value = [QueryHint(name = "jakarta.persistence.lock.timeout", value = "5000")])
   @Query("SELECT pd FROM PrisonerDetails pd WHERE pd.prisonerId = :id")
   fun findByIdForUpdate(id: String): Optional<PrisonerDetails>
 }
