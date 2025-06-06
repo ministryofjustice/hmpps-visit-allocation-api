f225fb8ab30ef0e1383b4234a1ac289c7c1d9dfd
Aaron Davies <168211160+adaviesMOJ@users.noreply.github.com>
2025-06-06 10:17:28 +0100

    Fix bug - Remove race condition on events which didn't change prisoner balance (#113)
    For some events we were calling to get the prisoner balance if NOMIS owned the prison.
    Even though the prisoner balance wasn't being adjusted. These events were often raised
    at the same time, causing us to increment the prisoner balance twice within a few milliseconds.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/enums/DomainEventType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/enums/DomainEventType.kt
@@ -4,7 +4,6 @@ package uk.gov.justice.digital.hmpps.visitallocationapi.enums
 enum class DomainEventType(val value: String) {
   CONVICTION_STATUS_UPDATED_EVENT_TYPE("prisoner-offender-search.prisoner.convicted-status-changed"),
   PRISONER_RECEIVED_EVENT_TYPE("prisoner-offender-search.prisoner.received"),
-  PRISONER_RELEASED_EVENT_TYPE("prison-offender-events.prisoner.released"),
   PRISONER_MERGED_EVENT_TYPE("prison-offender-events.prisoner.merged"),
   PRISONER_BOOKING_MOVED_EVENT_TYPE("prison-offender-events.prisoner.booking.moved"),
   VISIT_BOOKED_EVENT_TYPE("prison-visit.booked"),
--- src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/events/DomainEventsPrisonerConvictionStatusChangedTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/events/DomainEventsPrisonerConvictionStatusChangedTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.visitallocationapi.integration.events
 
-import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.kotlin.await
 import org.awaitility.kotlin.matches
 import org.awaitility.kotlin.untilAsserted
@@ -11,9 +10,7 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
-import org.springframework.http.HttpStatus
 import uk.gov.justice.digital.hmpps.visitallocationapi.enums.DomainEventType
-import uk.gov.justice.digital.hmpps.visitallocationapi.enums.VisitOrderStatus
 import uk.gov.justice.digital.hmpps.visitallocationapi.enums.VisitOrderType
 import uk.gov.justice.digital.hmpps.visitallocationapi.integration.wiremock.PrisonApiMockExtension.Companion.prisonApiMockServer
 import uk.gov.justice.digital.hmpps.visitallocationapi.integration.wiremock.PrisonerSearchMockExtension.Companion.prisonerSearchMockServer
@@ -52,71 +49,6 @@ class DomainEventsPrisonerConvictionStatusChangedTest : EventsIntegrationTestBas
     // Then
     await untilAsserted { verify(domainEventListenerSpy, times(1)).processMessage(any()) }
     await untilAsserted { verify(domainEventListenerServiceSpy, times(1)).handleMessage(any()) }
-    await untilAsserted { verify(nomisSyncService, times(1)).syncPrisonerBalanceFromEventChange(any(), any()) }
-    await untilAsserted { verify(changeLogService, times(1)).createLogSyncEventChange(any(), any()) }
-    await untilCallTo { domainEventsSqsClient.countMessagesOnQueue(domainEventsQueueUrl).get() } matches { it == 0 }
-
-    val visitOrders = visitOrderRepository.findAll()
-    assertThat(visitOrders.filter { it.status == VisitOrderStatus.AVAILABLE }.size).isEqualTo(5)
-  }
-
-  @Test
-  fun `when domain event prisoner conviction status changed is found, but an error is returned from prisoner search the message is sent to DLQ`() {
-    // Given
-    val prisonerId = "AA123456"
-    val prisonId = "HEI"
-
-    val prisoner = PrisonerDetails(prisonerId = prisonerId, lastVoAllocatedDate = LocalDate.now(), LocalDate.now())
-    prisoner.visitOrders.addAll(createVisitOrders(VisitOrderType.VO, 2, prisoner))
-    prisoner.visitOrders.addAll(createVisitOrders(VisitOrderType.PVO, 1, prisoner))
-    prisonerDetailsRepository.save(prisoner)
-
-    val domainEvent = createDomainEventJson(
-      DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value,
-      createPrisonerConvictionStatusChangedAdditionalInformationJson(prisonerId, prisonId),
-    )
-    val publishRequest = createDomainEventPublishRequest(DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value, domainEvent)
-
-    // And
-    prisonerSearchMockServer.stubGetPrisonerById(prisonerId = prisonerId, null, HttpStatus.NOT_FOUND)
-    prisonApiMockServer.stubGetVisitBalances(prisonerId = prisonerId, createVisitBalancesDto(0, 0))
-    prisonApiMockServer.stubGetPrisonEnabledForDps(prisonId, false)
-
-    // When
-    awsSnsClient.publish(publishRequest).get()
-
-    // Then
-    await untilCallTo { domainEventsSqsClient.countMessagesOnQueue(domainEventsQueueUrl).get() } matches { it == 0 }
-    await untilCallTo { domainEventsSqsDlqClient!!.countMessagesOnQueue(domainEventsDlqUrl!!).get() } matches { it == 1 }
-  }
-
-  @Test
-  fun `when domain event prisoner conviction status changed is found, but a 404 not found is returned from prison API then message is skipped`() {
-    // Given
-    val prisonerId = "AA123456"
-    val prisonId = "HEI"
-
-    val prisoner = PrisonerDetails(prisonerId = prisonerId, lastVoAllocatedDate = LocalDate.now(), LocalDate.now())
-    prisoner.visitOrders.addAll(createVisitOrders(VisitOrderType.VO, 2, prisoner))
-    prisoner.visitOrders.addAll(createVisitOrders(VisitOrderType.PVO, 1, prisoner))
-    prisonerDetailsRepository.save(prisoner)
-
-    val domainEvent = createDomainEventJson(
-      DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value,
-      createPrisonerConvictionStatusChangedAdditionalInformationJson(prisonerId, prisonId),
-    )
-    val publishRequest = createDomainEventPublishRequest(DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value, domainEvent)
-
-    // And
-    prisonerSearchMockServer.stubGetPrisonerById(prisonerId = prisonerId, createPrisonerDto(prisonerId = prisonerId, prisonId = prisonId, inOutStatus = "IN"))
-    prisonApiMockServer.stubGetVisitBalances(prisonerId = prisonerId, null, HttpStatus.NOT_FOUND)
-
-    // When
-    awsSnsClient.publish(publishRequest).get()
-
-    // Then
-    await untilCallTo { domainEventsSqsClient.countMessagesOnQueue(domainEventsQueueUrl).get() } matches { it == 0 }
-    await untilCallTo { domainEventsSqsDlqClient!!.countMessagesOnQueue(domainEventsDlqUrl!!).get() } matches { it == 0 }
   }
 
   @Test
@@ -150,70 +82,5 @@ class DomainEventsPrisonerConvictionStatusChangedTest : EventsIntegrationTestBas
     await untilCallTo { domainEventsSqsClient.countMessagesOnQueue(domainEventsQueueUrl).get() } matches { it == 0 }
 
     verifyNoInteractions(nomisSyncService)
-  }
-
-  @Test
-  fun `when domain event prisoner conviction status changed is found, then new prisoner balance is synced`() {
-    // Given
-    val prisonerId = "AA123456"
-    val prisonId = "HEI"
-
-    val domainEvent = createDomainEventJson(
-      DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value,
-      createPrisonerConvictionStatusChangedAdditionalInformationJson(prisonerId = prisonerId, convictedStatus = "Convicted"),
-    )
-    val publishRequest = createDomainEventPublishRequest(DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value, domainEvent)
-
-    // And
-    prisonerSearchMockServer.stubGetPrisonerById(prisonerId = prisonerId, createPrisonerDto(prisonerId = prisonerId, prisonId = prisonId, inOutStatus = "IN"))
-    prisonApiMockServer.stubGetVisitBalances(prisonerId = prisonerId, createVisitBalancesDto(3, 2))
-    prisonApiMockServer.stubGetPrisonEnabledForDps(prisonId, false)
-
-    // When
-    awsSnsClient.publish(publishRequest).get()
-
-    // Then
-    await untilAsserted { verify(domainEventListenerSpy, times(1)).processMessage(any()) }
-    await untilAsserted { verify(domainEventListenerServiceSpy, times(1)).handleMessage(any()) }
-    await untilAsserted { verify(nomisSyncService, times(1)).syncPrisonerBalanceFromEventChange(any(), any()) }
-    await untilAsserted { verify(changeLogService, times(1)).createLogSyncEventChange(any(), any()) }
-    await untilCallTo { domainEventsSqsClient.countMessagesOnQueue(domainEventsQueueUrl).get() } matches { it == 0 }
-
-    val visitOrders = visitOrderRepository.findAll()
-    assertThat(visitOrders.filter { it.status == VisitOrderStatus.AVAILABLE }.size).isEqualTo(5)
-
-    val prisonerDetails = prisonerDetailsRepository.findById(prisonerId).get()
-    assertThat(prisonerDetails.lastVoAllocatedDate).isEqualTo(LocalDate.now())
-    assertThat(prisonerDetails.lastPvoAllocatedDate).isNull()
-  }
-
-  @Test
-  fun `when domain event prisoner conviction status changed is found but 500 response from prison-api get service prison, then message fails`() {
-    // Given
-    val prisonerId = "AA123456"
-    val prisonId = "HEI"
-
-    val prisoner = PrisonerDetails(prisonerId = prisonerId, lastVoAllocatedDate = LocalDate.now(), LocalDate.now())
-    prisoner.visitOrders.addAll(createVisitOrders(VisitOrderType.VO, 2, prisoner))
-    prisoner.visitOrders.addAll(createVisitOrders(VisitOrderType.PVO, 1, prisoner))
-    prisonerDetailsRepository.save(prisoner)
-
-    val domainEvent = createDomainEventJson(
-      DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value,
-      createPrisonerConvictionStatusChangedAdditionalInformationJson(prisonerId = prisonerId, convictedStatus = "Convicted"),
-    )
-    val publishRequest = createDomainEventPublishRequest(DomainEventType.CONVICTION_STATUS_UPDATED_EVENT_TYPE.value, domainEvent)
-
-    // And
-    prisonerSearchMockServer.stubGetPrisonerById(prisonerId = prisonerId, createPrisonerDto(prisonerId = prisonerId, prisonId = prisonId, inOutStatus = "IN"))
-    prisonApiMockServer.stubGetVisitBalances(prisonerId = prisonerId, createVisitBalancesDto(3, 2))
-    prisonApiMockServer.stubGetPrisonEnabledForDps(prisonId, false, HttpStatus.INTERNAL_SERVER_ERROR)
-
-    // When
-    awsSnsClient.publish(publishRequest).get()
-
-    // Then
-    await untilCallTo { domainEventsSqsClient.countMessagesOnQueue(domainEventsQueueUrl).get() } matches { it == 0 }
-    await untilCallTo { domainEventsSqsDlqClient!!.countMessagesOnQueue(domainEventsDlqUrl!!).get() } matches { it == 1 }
   }
 }
