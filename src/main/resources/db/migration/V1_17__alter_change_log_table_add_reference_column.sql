0932f8c28103db7f4fee562845d21275be4e5bf7
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-06-09 12:30:38 +0100

    remove explicit begin commit from script

--- src/main/resources/db/migration/V1_17__alter_change_log_table_add_reference_column.sql
+++ src/main/resources/db/migration/V1_17__alter_change_log_table_add_reference_column.sql
@@ -1,7 +1,3 @@
-BEGIN;
-
 ALTER TABLE change_log ADD COLUMN reference UUID;
 UPDATE change_log SET reference = gen_random_uuid() WHERE reference IS NULL;
 ALTER TABLE change_log ALTER COLUMN reference SET NOT NULL;
-
-COMMIT;
