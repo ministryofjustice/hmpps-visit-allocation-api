29175188412584f4a7bd7664b291a5dd9fea38c2
adaviesMOJ <aaron.moriarty@digital.justice.gov.uk>
2025-12-12 08:54:52 +0000

    Prisoner merge events - change domain event raised after processing
    Instead of an adjustment event, we raise a balance reset event. This is
    needed, because we have no knowledge of the booking balances of each prisoner
    on NOMIS's side. Instead of raising an adjustment, which can cause NOMIS to
    fall out of sync.
    
    The balance-reset event, forces NOMIS to call to get the current balance, and sync
    to it, this fixes our issue.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/events/DomainEventsPrisonerMergedTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/visitallocationapi/integration/events/DomainEventsPrisonerMergedTest.kt
@@ -208,7 +208,7 @@ class DomainEventsPrisonerMergedTest : EventsIntegrationTestBase() {
     // prisoner should end up wth 5 VOs and 8 PVOs
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.VO }.size).isEqualTo(5)
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.PVO }.size).isEqualTo(8)
-    verify(snsService, times(1)).sendPrisonAllocationAdjustmentCreatedEvent(any())
+    verify(snsService, times(1)).sendPrisonAllocationPrisonerBalanceResetEvent(any())
   }
 
   @Test
@@ -253,7 +253,7 @@ class DomainEventsPrisonerMergedTest : EventsIntegrationTestBase() {
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.VO }.size).isEqualTo(2)
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.PVO }.size).isEqualTo(3)
     verify(changeLogService, times(0)).createLogAllocationForPrisonerMerge(any(), any(), any())
-    verify(snsService, times(0)).sendPrisonAllocationAdjustmentCreatedEvent(any())
+    verify(snsService, times(0)).sendPrisonAllocationPrisonerBalanceResetEvent(any())
   }
 
   @Test
@@ -295,7 +295,7 @@ class DomainEventsPrisonerMergedTest : EventsIntegrationTestBase() {
     // prisoner should end up wth 3 VOs and 5 PVOs
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.VO }.size).isEqualTo(3)
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.PVO }.size).isEqualTo(5)
-    verify(snsService, times(1)).sendPrisonAllocationAdjustmentCreatedEvent(any())
+    verify(snsService, times(1)).sendPrisonAllocationPrisonerBalanceResetEvent(any())
   }
 
   @Test
@@ -331,7 +331,7 @@ class DomainEventsPrisonerMergedTest : EventsIntegrationTestBase() {
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.VO }.size).isEqualTo(0)
     assertThat(availableVisitOrdersForPrisoner.filter { it.type == VisitOrderType.PVO }.size).isEqualTo(0)
     verify(changeLogService, times(0)).createLogAllocationForPrisonerMerge(any(), any(), any())
-    verify(snsService, times(0)).sendPrisonAllocationAdjustmentCreatedEvent(any())
+    verify(snsService, times(0)).sendPrisonAllocationPrisonerBalanceResetEvent(any())
   }
 
   @Test
