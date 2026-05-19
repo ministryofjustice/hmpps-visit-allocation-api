42a611a837f851cbf5c74670965ea1db93ead276
Kristian Gregory <78143888+KristianGregor@users.noreply.github.com>
2026-05-19 08:48:37 +0100

    bump kotlin lib to a beta version that is proxy aware (#307)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.2.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.3.0-beta")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
